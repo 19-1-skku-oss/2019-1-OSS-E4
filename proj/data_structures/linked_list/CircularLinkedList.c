88f572c60ea51f776d2e983443b0b85420a8b1dc
CheFly1116 <sam4363@naver.com>
2019-06-11 02:56:04 +0900

    Update CircularLinkedList.c

--- proj/data_structures/linked_list/CircularLinkedList.c
+++ proj/data_structures/linked_list/CircularLinkedList.c
@@ -7,7 +7,7 @@ typedef struct __Node
 {
     Data item;
     struct __Node *next;
-}
+} Node;
 
 typedef struct
 {
@@ -15,6 +15,7 @@ typedef struct
     int len;
 } CircularList;
 
+void InitList(CircularList *plist);
 void InsertInitItem(CircularList *plist, Data item);
 void InsertFirst(CircularList *plist, Data item);
 void InsertLast(CircularList *plist, Data item);
@@ -23,11 +24,21 @@ void RemoveInitItem(CircularList *plist);
 void RemoveFirst(CircularList *plist);
 void RemoveLast(CircularList *plist);
 void RemoveMiddle(CircularList *plist, int pos);
+void PrintList(CircularList *plist);
 
 int main(void)
 {
-
-
+    CircularList list;
+    InitList(&list);
+    InsertFirst(&list, 1);
+    InsertLast(&list, 2);
+    InsertLast(&list, 4);
+    InsertMiddle(&list, 2, 3);
+    PrintList(&list);
+    RemoveFirst(&list);
+    RemoveLast(&list);
+    PrintList(&list);
+    return 0;
 }
 
 void InsertInitItem(CircularList *plist, Data item)
@@ -142,5 +153,14 @@ void RemoveLast(CiruclarList *plist, int pos)
         free(temp);
         plist->tail = cur;
         plist->len--;
+    }
+}
+
+void PrintList(CircularList *plist)
+{
+    Node *temp = plist->tail;
+    for (int i = 0; i <plist->len; i++) {
+        temp = temp->next;
+        printf("%d", temp->item);
     }
 }
